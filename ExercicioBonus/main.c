842dc448bf371615ea5fd5a51c45a46e07a0b82c
rmmenezes <ra29fa@gmail.com>
2018-03-25 19:26:23 -0300

    -

--- ExercicioBonus/main.c
+++ ExercicioBonus/main.c
@@ -1,18 +1,15 @@
 #include <GL/glut.h>
 
 int init(void){
-    glClearColor(1.0, 1.0, 1.0, 0.0);     //define a cor de fundo
+    glClearColor(1.0, 1.0, 1.0, 1.0);     //define a cor de fundo
 
     glMatrixMode(GL_PROJECTION);          //carrega a matriz de projeção
     gluOrtho2D(0.0,100.0,0.0,100.0);      //define projeção ortogonal 2D
 
 }
 
-void display(void){
-    glClear(GL_COLOR_BUFFER_BIT);         //desenha o fundo (limpa a janela)
-    
-    glColor3f(1.0,0.0,0.0);               //altera o atributo de cor
-    glBegin(GL_POLYGON);                    
+void object(){
+	glBegin(GL_POLYGON);                    
         glVertex2i(0,0);
         
         glVertex2i(0,10);
@@ -21,12 +18,73 @@ void display(void){
 
         glVertex2i(10,0);
     glEnd();
+}
 
+
+void display(void){
+
+    glClear(GL_COLOR_BUFFER_BIT);         //desenha o fundo (limpa a janela)
+    glColor3f(1.0,0.0,0.0);               //altera o atributo de cor
+    object();
+
+
+    glPushMatrix();
+    glPopMatrix();
     glFlush();                            //desenha os comandos não executados
+}
+
+void specialTranslation(int key, int x, int y){
+	switch (key){
+		case GLUT_KEY_UP:
+			glTranslatef(0,1,0);
+			glutPostRedisplay();
+		break;
+		case GLUT_KEY_DOWN:
+			glTranslatef(0,-1,0);
+			glutPostRedisplay();
+		break;
+		case GLUT_KEY_LEFT:
+			glTranslatef(-1,0,0);
+			glutPostRedisplay();
+		break;
+		case GLUT_KEY_RIGHT:
+			glTranslatef(1,0,0);
+			glutPostRedisplay();
+		break;
+	}
+}
+
+void specialRotation(int key, int x, int y){
+	switch (key){
+		case GLUT_KEY_LEFT:
+			glRotatef(1,0,0,1);
+			glutPostRedisplay();
+		break;
+		case GLUT_KEY_RIGHT:
+			glRotatef(-1,0,0,1);
+			glutPostRedisplay();
+		break;
+	}
+}
+
+
+void keyboardSimple(unsigned char key, int x, int y){
+	switch (key){
+		case 'q':
+			exit(0);
+		break;
+		case 't':
+			glutSpecialFunc(specialTranslation); 	//Ativa Traslação
+		break;
+		case 'r':
+			glutSpecialFunc(specialRotation);		//Ativa Rotação
+		break;
+	}
 }
 
 
 int main(int argc, char** argv) {
+
 
     glutInit(&argc,argv);                                     //inicializa o GLUT
     glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB);               //configura o modo de display
@@ -36,6 +94,9 @@ int main(int argc, char** argv) {
 
     init();                          //executa função de inicialização
     glutDisplayFunc(display);        //estabelece a função "display" como a função callback de exibição.
+    glutKeyboardFunc(keyboardSimple);
+   
+
     glutMainLoop();                  //mostre tudo e espere
     return 0;
 }
